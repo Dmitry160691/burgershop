079d42dd0fb102ceea03af0e71e5779310afee6c
Dmitry160691 <160691@mail.ru>
2025-03-17 20:25:08 +0300

    Add scrollbar

--- src/components/ingredient-category/ingredient-category.tsx
+++ src/components/ingredient-category/ingredient-category.tsx
@@ -3,20 +3,22 @@ import s from './ingredient-category.module.scss';
 import { IngredientCard } from '@components/ingredient-card/ingredient-card';
 
 type IngredientCategoryProps = {
-    title: string;
-    items: any[];
+	title: string;
+	items: any[];
 };
 
-export const IngredientCategory: FC<IngredientCategoryProps> = ({ title, items }) => {
-    
-    return (
-        <>
-            <h2 className='mt-10 text_type_main-medium'>{title}</h2>
-            <div className={`ml-3 mr-3 mb-3 ${s['flex-container']}`}>
-            {items.map((item, index) => 
-                <IngredientCard key={index} item={item}/>
-            )}
-            </div>
-        </> 
-    );
-}
+export const IngredientCategory: FC<IngredientCategoryProps> = ({
+	title,
+	items,
+}) => {
+	return (
+		<>
+			<h2 className='mt-10 text_type_main-medium'>{title}</h2>
+			<div className={`ml-3 mr-3 mb-3 ${s['flex-container']}`}>
+				{items.map((item, index) => (
+					<IngredientCard key={index} item={item} />
+				))}
+			</div>
+		</>
+	);
+};
